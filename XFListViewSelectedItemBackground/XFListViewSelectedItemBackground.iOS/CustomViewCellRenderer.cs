7a48a958eeef67375f709156c955ef7b68be0c0e
Alessandro Del Sole <alessandro.delsole@visual-basic.it>
2018-03-01 16:55:27 +0100

    Add project files.

---
+++ XFListViewSelectedItemBackground/XFListViewSelectedItemBackground.iOS/CustomViewCellRenderer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using XFListViewSelectedItemBackground;
+using XFListViewSelectedItemBackground.iOS;
+
+[assembly: ExportRenderer(typeof(CustomViewCell), typeof(CustomViewCellRenderer))]
+namespace XFListViewSelectedItemBackground.iOS
+{
+    public class CustomViewCellRenderer : ViewCellRenderer
+    {
+        public override UITableViewCell GetCell(Cell item, UITableViewCell reusableCell, UITableView tv)
+        {
+            var cell = base.GetCell(item, reusableCell, tv);
+
+            ////cell.SelectionStyle = UITableViewCellSelectionStyle.Blue;
+            var selectedBackgroundView = new UIView(cell.SelectedBackgroundView.Bounds);
+            selectedBackgroundView.Layer.BackgroundColor = Color.FromRgb(127, 210, 255).ToCGColor(); 
+            cell.SelectedBackgroundView = selectedBackgroundView;
+
+            item.ForceUpdateSize();
+            return cell;
+        }
+
+    }
+}
---
+++ XFListViewSelectedItemBackground/XFListViewSelectedItemBackground/CustomViewCell.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XFListViewSelectedItemBackground
+{
+    public class CustomViewCell: ViewCell
+    {
+
+    }
+}
